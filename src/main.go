852d611fdd28be8008f1dc6315e7b71b0c9dd8d8
Rotfuchs-von-Vulpes <derfuchs.pa@gmail.com>
2025-09-28 23:27:22 -0400

    reply dont trigger mention reaction

--- src/main.go
+++ src/main.go
@@ -624,9 +624,9 @@ func main() {
 		if message.MentionEveryone {
 			discord.ChannelMessageSend(channelID, "<:memojo_really:1411209850213498890>")
 		} else {
-			if len(message.Mentions) == 1 && message.Mentions[0].ID == discord.State.User.ID {
+			if len(message.Mentions) == 1 && message.Type != discordgo.MessageTypeReply && message.Mentions[0].ID == discord.State.User.ID {
 				discord.ChannelMessageSend(channelID, "<a:foxexcite:1421359331361816678>")
-			} else if len(message.Mentions) > 1 {
+			} else if len(message.Mentions) > 1 && message.Type != discordgo.MessageTypeReply {
 				for _, user := range message.Mentions {
 					if user.ID == discord.State.User.ID {
 						discord.ChannelMessageSend(channelID, "<:pepe_think:1421357826051407962>")
