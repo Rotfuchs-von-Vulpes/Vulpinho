d1ab0211baf89dbc55ab5a3cfa16cdb06981da35
Rotfuchs-von-Vulpes <derfuchs.pa@gmail.com>
2025-10-06 22:24:58 -0400

    add words to fops list

--- src/main.go
+++ src/main.go
@@ -438,7 +438,7 @@ func main() {
 		}
 
 		words := strings.Split(strings.ToLower(message.Content), " ")
-		fops_list := []string{"raposa", "raposo", "raposinha", "raposinhas", "raposas", "raposos", "fop", "fops", "fox", "poposa", "poposas", "foxes", "fxoe"}
+		fops_list := []string{"vulpinho", "raposa", "raposo", "raposinha", "raposinhas", "raposas", "raposos", "fop", "fops", "fox", "poposa", "poposas", "poposo", "poposos", "foxes", "fxoe"}
 
 		if len(words) >= 1 {
 			word_minus, found := strings.CutSuffix(words[0], "!")
