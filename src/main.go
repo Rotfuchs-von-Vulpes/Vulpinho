026e233f41af6511d4bc23d2f5d1af36ce2a127d
Rotfuchs-von-Vulpes <derfuchs.pa@gmail.com>
2025-10-14 22:18:19 -0400

    add fox unicode to fops_list

--- src/main.go
+++ src/main.go
@@ -242,7 +242,7 @@ func main() {
 		}
 
 		words := strings.Split(strings.ToLower(message.Content), " ")
-		fops_list := []string{"vulpinho", "raposa", "raposo", "raposinha", "raposinhas", "raposas", "raposos", "fop", "fops", "fox", "poposa", "poposas", "poposo", "poposos", "foxes", "fxoe"}
+		fops_list := []string{"vulpinho", "🦊", "raposa", "raposo", "raposinha", "raposinhas", "raposas", "raposos", "fop", "fops", "fox", "poposa", "poposas", "poposo", "poposos", "foxes", "fxoe"}
 
 		if len(words) >= 1 {
 			word_minus, found := strings.CutSuffix(words[0], "!")
