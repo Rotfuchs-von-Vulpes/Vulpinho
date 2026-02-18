d014ca7921bd1ab99753ccc145b92f6eb2dbdbf2
Rotfuchs-von-Vulpes <derfuchs.pa@gmail.com>
2026-02-17 21:48:53 -0400

    minor add

--- main.go
+++ main.go
@@ -187,7 +187,7 @@ func main() {
 		}
 
 		words := strings.Split(strings.ToLower(message.Content), " ")
-		fops_list := []string{"vulpinho", "🦊", "raposa", "raposo", "raposinha", "raposinhas", "raposas", "raposos", "fop", "fops", "fopse", "fopses", "fox", "poposa", "poposas", "poposo", "poposos", "foxes", "fxoe", "fxoes"}
+		fops_list := []string{"vulpinho", "🦊", "raposa", "raposo", "raposinha", "raposinhas", "raposas", "raposos", "fop", "fops", "fopse", "fopses", "fox", "poposa", "poposas", "poposo", "poposos", "foxes", "fxoe", "fxoes", "semimi"}
 
 		if len(words) >= 1 {
 			word_minus, found := strings.CutSuffix(words[0], "!")
