c148e9ac2e89499bbeda2657fdb2543170e960ec
Rotfuchs-von-Vulpes <derfuchs.pa@gmail.com>
2026-02-19 01:48:01 -0400

    add lojban word to fops list

--- main.go
+++ main.go
@@ -206,7 +206,7 @@ func main() {
 		}
 
 		words := strings.Split(strings.ToLower(message.Content), " ")
-		fops_list := []string{"vulpinho", "🦊", "raposa", "raposo", "raposinha", "raposinhas", "raposas", "raposos", "fop", "fops", "fopse", "fopses", "fox", "poposa", "poposas", "poposo", "poposos", "foxes", "fxoe", "fxoes", "semimi"}
+		fops_list := []string{"vulpinho", "🦊", "raposa", "raposo", "raposinha", "raposinhas", "raposas", "raposos", "fop", "fops", "fopse", "fopses", "fox", "poposa", "poposas", "poposo", "poposos", "foxes", "fxoe", "fxoes", "semimi", "lorxu"}
 
 		if len(words) >= 1 {
 			word_minus, found := strings.CutSuffix(words[0], "!")
