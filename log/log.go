4b3b694641e05aacce8aac11c7aae9c3ca968eac
Rotfuchs-von-Vulpes <derfuchs.pa@gmail.com>
2026-02-27 00:33:53 -0400

    clean

--- log/log.go
+++ log/log.go
@@ -10,7 +10,7 @@ var logFile *os.File
 func Init() bool {
 	logFile, err := os.Create("log/log.txt")
 	if err != nil {
-		slog.Error("Erro ao inciar", "erro", err.Error())
+		slog.Error("Inicio do sistema de log falhou.", "erro", err.Error())
 		return true
 	}
 	logger := slog.New(slog.NewMultiHandler(slog.NewTextHandler(os.Stdout, nil), slog.NewJSONHandler(logFile, nil)))
